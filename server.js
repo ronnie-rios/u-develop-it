a1eeec303286a3d82405b1995221e82fe827b249
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-02 12:04:21 -0500

    create, delete, and get candidate queries added

--- server.js
+++ server.js
@@ -21,10 +21,35 @@ const db = mysql.createConnection(
 console.log('connected to the election database.')
 )
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     //console.log(rows);
+// });
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
 
+//   // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 //default response for any other request (404 not found)
 app.use((req, res) => {
     res.status(404).end();
